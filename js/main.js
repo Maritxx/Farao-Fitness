938bacb9cc9262e60f452e52d58d77c6b67e5ac5
Marit <maritdevries1@live.nl>
2023-10-11 11:19:14 +0200

    Added animation and overlay for mobile-navigation toggle

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 /*Mobile Menu*/
 const menuToggleButton = document.getElementById("navIcon");
 const menuDropdown = document.getElementById("navDropdown");
+const menuOverlay = document.getElementById("dropdownOverlay");
 
 function toggleMobileMenu() {
     menuToggleButton.classList.toggle("expanded-button");
     menuDropdown.classList.toggle("expanded-dropdown");
 
     menuToggleButton.classList.contains("expanded-button") ? menuToggleButton.setAttribute("aria-expanded", true) : menuToggleButton.setAttribute("aria-expanded", false);
+
+    if (menuToggleButton.classList.contains("expanded-button")) {
+        menuOverlay.style.display = "block";
+        document.getElementsByTagName("html")[0].style.overflowY = "hidden";
+    } else {
+        menuOverlay.style.display = "none";
+        document.getElementsByTagName("html")[0].style.overflowY = "scroll";
+    }
 }
 
 
