d80f289313f9eac52257cb59755de46ab07c5fc1
Marit <maritdevries1@live.nl>
2023-11-08 17:21:24 +0100

    CSS Kosten page and select-package functionality added in JS

--- js/main.js
+++ js/main.js
@@ -21,4 +21,46 @@ function toggleMobileMenu() {
 
 menuToggleButton.addEventListener("click", ((e) => {
     toggleMobileMenu();
-}))
+}));
+
+
+/*Packages Selection*/
+function changePrice(selectedMonths, clickedButton) {
+    const budgetPriceElement = document.getElementById("budgetPrice");
+    const fullServicePriceElement = document.getElementById("fullServicePrice");
+    const tryOutTextElement = document.getElementById("tryOutText");
+    const allCostButtons = document.getElementsByClassName("button__kosten");
+
+    Array.from(allCostButtons).forEach((button) => {
+        if (button.classList.contains("button__kosten--active")) {
+            button.classList.remove("button__kosten--active");
+        }
+    })
+
+    switch(selectedMonths) {
+        case 1:
+            budgetPriceElement.textContent = "30";
+            fullServicePriceElement.textContent = "50";
+            clickedButton.classList.add("button__kosten--active");
+            tryOutTextElement.style.visibility = "visible";
+            break;
+        case 3:
+            budgetPriceElement.textContent = "150";
+            fullServicePriceElement.textContent = "200";
+            clickedButton.classList.add("button__kosten--active");
+            tryOutTextElement.style.visibility = "hidden";
+            break;
+        case 6:
+            budgetPriceElement.textContent = "250";
+            fullServicePriceElement.textContent = "350";
+            clickedButton.classList.add("button__kosten--active");
+            tryOutTextElement.style.visibility = "hidden";
+            break;
+        case 12:
+            budgetPriceElement.textContent = "400";
+            fullServicePriceElement.textContent = "600";
+            clickedButton.classList.add("button__kosten--active");
+            tryOutTextElement.style.visibility = "hidden";
+            break;
+    }
+}
